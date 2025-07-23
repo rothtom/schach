92e8fe10bbd3c8b369cb8b9520a02e826aa11928
Tom Roth <t.roth11@icloud.com>
2025-07-23 15:19:11 +0200

    about to start over

--- include/colors.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-enum colors {
-    BLACK,
-    WHITE
-};
--- src/Coordinates.cpp
+++
@@ -1,5 +0,0 @@
-#include "Coordinates.hpp"
-
-bool operator<(const ChessCoordinates& l, const ChessCoordinates& r) {
-    return (l.collumn<r.collumn || (l.collumn==r.collumn && l.row<r.row));
-}
